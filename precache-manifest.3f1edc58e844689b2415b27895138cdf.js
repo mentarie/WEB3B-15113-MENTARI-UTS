4c564e7204887881e4b9b1009e0f33253d0b05d6
mentari <mentari.e.r@mail.ugm.ac.id>
2020-04-15 14:44:50 +0700

    Updates

---
+++ precache-manifest.3f1edc58e844689b2415b27895138cdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1b59fdf78de85712175aeac1311eaa",
+    "url": "/WEB3-15113-MENTARI-UTS/index.html"
+  },
+  {
+    "revision": "fa3cd281e64d1e4b56c2",
+    "url": "/WEB3-15113-MENTARI-UTS/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "81f4c8b5795bc6c09d27",
+    "url": "/WEB3-15113-MENTARI-UTS/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa3cd281e64d1e4b56c2",
+    "url": "/WEB3-15113-MENTARI-UTS/static/js/2.fe684f2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WEB3-15113-MENTARI-UTS/static/js/2.fe684f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f4c8b5795bc6c09d27",
+    "url": "/WEB3-15113-MENTARI-UTS/static/js/main.7de3f2a3.chunk.js"
+  },
+  {
+    "revision": "15987995b5db3fa8dd5e",
+    "url": "/WEB3-15113-MENTARI-UTS/static/js/runtime-main.d282c632.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/WEB3-15113-MENTARI-UTS/precache-manifest.3f1edc58e844689b2415b27895138cdf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WEB3-15113-MENTARI-UTS/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
